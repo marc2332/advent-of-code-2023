2725bfed7971b18068b94921e5d8148cdec509c8
marc2332 <mespinsanz@gmail.com>
2023-12-01 09:08:14 +0100

    first day

---
+++ day1/first.js
@@ -0,0 +1,25 @@
+const input = await Deno.readTextFile("input", "UTF-8")
+
+const res =input.trim().split("\n")
+.map(text => {
+    let first = null;
+
+    for(const char of text){
+        if(!first && !Number.isNaN(Number(char))){
+            first = char
+        }
+    }
+
+    let second = null;
+
+    for(const char of text.split("").reverse()){
+        if(!second && !Number.isNaN(Number(char))){
+            second = char
+        }
+    }
+
+    return Number(`${first}${second}`)
+})
+    .reduce((acc, n) => acc + n, 0)
+
+console.log(res)
